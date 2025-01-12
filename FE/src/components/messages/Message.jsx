d8beab6b206057f94d74365d1f0e7134d0232e1c
lamiaa_emad <84296897+Lamiaaa867@users.noreply.github.com>
2025-01-12 15:55:23 +0200

    final commit

--- FE/src/components/messages/Message.jsx
+++ FE/src/components/messages/Message.jsx
@@ -5,33 +5,27 @@ import useConversation from '../../zustand/useConverstion.js'
 const Message = ({message}) => {
   console.log("your message is " , message)
   const { authUser } = useAuthContext();
-  console.log("your auuuthhhh is " , authUser._id)
   const { selectedConversation } = useConversation();
-  console.log("your selectedConversationkkkkkk is " , selectedConversation)
-  const fromMe = message?.senderId === authUser?._id;
+  const fromMe = message.senderId === authUser._id;
+  const shakeClass = message.shouldShake ? "shake" : "";
+
   const chatClassName = fromMe ? "chat-end" : "chat-start";
   const profilePic = fromMe
     ? authUser.profilePicture
     : selectedConversation.profilePicture;
-    console.log("your selectedConversationprofilePicture is " , selectedConversation.profilePicture)
-  const bubbleBgColor = fromMe ? "" : "bg-blue-500";
+     const bubbleBgColor = fromMe ? "bg-blue-500" : "";
   
 
   return (
     <div className={`chat ${chatClassName}`}>
-      <div className="chat-image avatar">
-        <div className="w-10 rounded-full">
-          <img alt="User avatar" src={profilePic} />
-        </div>
-      </div>
-      {/* Accessing the 'message' property directly */}
-      <div className={`chat-bubble text-white ${bubbleBgColor} pb-2`}>
-        {message?.message}
-      </div>
-      <div className="chat-footer opacity-50 text-xs flex gap-1 items-center">
-        {message?.createdAt}
+    <div className='chat-image avatar'>
+      <div className='w-10 rounded-full'>
+        <img alt='Tailwind CSS chat bubble component' src={profilePic} />
       </div>
     </div>
+    <div className={`chat-bubble text-white ${shakeClass} ${bubbleBgColor} pb-2`}>{message.message}</div>
+    <div className='chat-footer opacity-50 text-xs flex gap-1 items-center'>{message.createdAt}</div>
+  </div>
   );
 };
 
